facd1641477db1ec573fbf3be874215abd06b763
ryanjames1011 <ryanjames6789@outlook.com>
2021-11-11 10:55:17 +0000

    added comments

--- .venv/app.py
+++ .venv/app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template
 
-# Configure application
+# configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
+# ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# index page 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# the login page
 @app.route("/login")
 def login():
     return render_template("login.html")
 
+# make an account page
 @app.route("/create")
 def create():
     return render_template("create.html")
 
+# continue with no account
 @app.route("/noacc")
 def noacc():
     return render_template("noacc.html")
 
+# the actual chatbot page
 @app.route("/botpage")
 def botpage():
     return render_template("botpage.html")
